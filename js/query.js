527a7cffa5232705a7d7895fa25a0603fa2fca74
aerix <vetalaerix@gmail.com>
2015-02-18 20:21:39 +0200

    Deleted unnecessary lines

--- js/query.js
+++ js/query.js
@@ -11,10 +11,10 @@ function query($scope, $http)
     // $scope.request.password = "";
     // $scope.database = "";
     $scope.request.sql = "";
-    
+
     $scope.executing = false; // Request is now executing?
     $scope.error = new Object();
-	
+
 	$scope.options = new Object();
 	$scope.options.show = false;
 	$scope.options.swing = function()
@@ -22,40 +22,39 @@ function query($scope, $http)
 		$scope.options.show = !$scope.options.show;
 	};
 	
-	
+
     /** Execute query. */
     $scope.execute = function()
-    {
+	{
         // Prepare.
         $scope.executing = true;
         delete($scope.response); // Clear previous successful result (if it was).
 		$scope.error.show = false;
-               
+
         // Send.
         $http.post("db/", $scope.request)
-        .success
+			.success
         (
         	function(response)
-        	{
+			{
 				$scope.response = new Object();
+
 				// Names of table columns. From first result row.
 				$scope.response.header = Object.keys(response[0]);
+
 				// Table cells.
-        	    $scope.response.data = response;    
-                
+				$scope.response.data = response;
+
                 $scope.executing = false;
         	}
         )
-        .error
+			.error
         (
             function (message, status)
-            {
+			{
 				$scope.executing = false;
 				$scope.error.show = true;
-				$scope.error.message = message;
-				
-                // Display error message.
-                // alert(message);            
+				$scope.error.message = message;    
                 console.log(status, message);
             }
         );    
